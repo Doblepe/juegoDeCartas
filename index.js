f40f15c06fec5cea8fedc42b3780a13931a3b3b1
Doblepe <Vicbleperez@gmail.com>
2021-05-30 17:09:42 +0200

    Creemos que funciona

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const URL_MONGO =
 const express = require('express');
 const mongodb = require('mongodb');
 const app = express();
-let MongoClient = mongodb.MongoClient;
+// let MongoClient = mongodb.MongoClient;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 let db;
-MongoClient.connect(URL_MONGO, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
+/* MongoClient.connect(URL_MONGO, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
 	err ? console.log(err) : (db = client.db('Cards-game'));
 });
 app.listen(process.env.PORT || 3000);
 
 const URL_MONGO =
-	'mongodb+srv://vicdoblepe:vicdoblepe2@cards-game.skn15.mongodb.net/Cards-game?retryWrites=true&w=majority';
+	'mongodb+srv://vicdoblepe:vicdoblepe2@cards-game.skn15.mongodb.net/Cards-game?retryWrites=true&w=majority'; */
+
+const MongoClient = require('mongodb').MongoClient;
+const uri =
+	'mongodb+srv://vicdoblepe:<password>@cards-game.skn15.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+	const collection = client.db('cards-game').collection('users');
+	// perform actions on the collection object
+	client.close();
+});
